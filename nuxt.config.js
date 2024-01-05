726b8f357db82d41b2a3cf9a614d141a6544ec73
TC Wang <wangtct@hotmail.com>
2024-01-05 17:59:00 +0800

    navbar

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,16 @@ export default {
     ],
     script: [
       {
+        src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+      },
+      {
         src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
       },
     ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["bootstrap/dist/css/bootstrap.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
